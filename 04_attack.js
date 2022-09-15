b3aef28c708b6798766ab73d5326aa4303838239
SSzretter <SSzretter@users.noreply.github.com>
2022-09-15 17:16:08 -0400

    resting code working

--- 04_attack.js
+++ 04_attack.js
@@ -7,6 +7,13 @@ function attackMonster(monster) {
     return;
   }
 
+  let hostileMonster = get_nearest_monster({ target: 'codedev' });
+  if ((isBelowPercent(character.mp, character.max_mp, manaThreshold) || isBelowPercent(character.hp, character.max_hp, healthThreshold)) && target && hostileMonster !== target) // low on hp and mp so if we have a target and it is NOT hostile clear the target so we can rest
+  {
+    target = null;
+    set_message("Resting");
+  }
+
   if (!in_attack_range(target)) {
     if (parent.walkingActive) {
       walkHalfwayToTarget(target);
@@ -21,7 +28,6 @@ function attackMonster(monster) {
 function getTargetMonster(monster) {
   let target = get_targeted_monster();
   if (!target) { // we need a new monster
-    if(isAbovePercent(character.mp, character.max_mp, manaThreshold)) // only target a monster if we have enough mp, otherwise wait and recover mp
       target = get_nearest_monster(monster);
     if (target) change_target(target);
   }
