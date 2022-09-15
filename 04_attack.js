bce7712855229d8ded09ad5ab79074b86867c104
SSzretter <SSzretter@users.noreply.github.com>
2022-09-15 16:11:12 -0400

    recover mp instead of new target monster

--- 04_attack.js
+++ 04_attack.js
@@ -20,8 +20,9 @@ function attackMonster(monster) {
 
 function getTargetMonster(monster) {
   let target = get_targeted_monster();
-  if (!target) {
-    target = get_nearest_monster(monster);
+  if (!target) { // we need a new monster
+    if(isAbovePercent(character.mp, character.max_mp, manaThreshold)) // only target a monster if we have enough mp, otherwise wait and recover mp
+      target = get_nearest_monster(monster);
     if (target) change_target(target);
   }
   return target;
